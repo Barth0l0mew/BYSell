4aec70c55a4164bc77099e76b70cb1afa89fb402
Dima <barth0l0mew@mail.ru>
2024-07-24 20:15:25 +0300

    Create SpringBoot app

---
+++ src/main/java/com/example/BYSell/BySellApplication.java
@@ -0,0 +1,13 @@
+package com.example.BYSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BySellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BySellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BYSell/BySellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BYSell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BySellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
